53580b77390aa307a2b390ddf85a2bfa92bc3c5e
GeekMasher <geekmasher@gmail.com>
2022-12-21 12:24:38 +0000

    Add web server backend template using Rocket

---
+++ web-server/src/main.rs
@@ -0,0 +1,25 @@
+#[macro_use] extern crate rocket;
+
+use log::{info, error};
+
+mod routes;
+use routes::index;
+
+#[rocket::main]
+async fn main() {
+    env_logger::init();
+
+    info!("Building Rocket");
+    let rocket = rocket::build()
+        .mount("/", routes![index]);
+    
+    // TODO: Add routes
+
+
+    if let Err(e) = rocket.launch().await {
+        error!("Error launching Rocket: {}", e);
+        drop(e);
+    }
+
+    info!("Stopping Rocket")
+}
---
+++ web-server/src/routes.rs
@@ -0,0 +1,7 @@
+
+
+#[get("/")]
+pub fn index() -> &'static str {
+    "Hello, world!"
+}
+
